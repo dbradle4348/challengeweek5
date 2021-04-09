fb289f7f2f250e0e14ad889503fe0cdca1531871
dbradle4348 <73002887+dbradle4348@users.noreply.github.com>
2021-04-09 17:00:40 -0600

    still not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 // current date and time
-var d = new Date();
-document.getElementById("demo").innerHTML = d.getDate();
+var today = new Date();
+var dd = today.getDate();
+
+var mm = today.getMonth()+1; 
+var yyyy = today.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+today = mm+'-'+dd+'-'+yyyy;
+document.getElementById("currentDay").innerHTML = today
 
 jQuery(function() {
     $("saveBtn").on("click", function () {
@@ -11,7 +25,8 @@ jQuery(function() {
     })
 
 function tracker() {
-    var rightNow = moment().hour(); 
+    var rightNow = moment().hours();
+
 
     //loop time blocks
     $(".time-block").each(function () {
